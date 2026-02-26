ddd277cb44f823835c3013100cf5488b0c1db94e
ederct11 <edercardona@hotmail.com>
2026-02-26 00:15:56 -0500

    Eliminar archivos sueltos de GitHub

--- Proyecto/workers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .worker_registry import WorkerRegistry
-from .monitored_redis_worker import MonitoredRedisWorker
-from .redis_task_queue_v2 import RedisTaskQueueV2
-
-__all__ = ['WorkerRegistry', 'MonitoredRedisWorker', 'RedisTaskQueueV2']
-
